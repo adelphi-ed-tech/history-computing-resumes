0d20e1e16020d2946ca79c9db296dfb411e095a5
matthew x. curinga <matt@curinga.com>
2023-07-05 14:29:38 -0400

    Initial commit

---
+++ src/Al_Khwarizmi.js
@@ -0,0 +1,9 @@
+function Al_Khwarizmi() {
+  return (
+    <div>
+      <h1>Al-Khwarizmi</h1>
+    </div>
+  );
+}
+
+export default Al_Khwarizmi;
---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { Outlet, Link, Routes, Route } from "react-router-dom";
+
+import Al_Khwarizmi from "./Al_Khwarizmi";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index path="/" element={<Directory />} />
+          <Route path="al-khwarizmi" element={<Al_Khwarizmi />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Main() {
+  return (
+    <div>
+      <header>
+        <h1>
+          <Link to="/">History of Computing Resumes</Link>
+        </h1>
+      </header>
+      <Outlet />
+    </div>
+  );
+}
+
+function Directory() {
+  return (
+    <div>
+      <ol>
+        <li>
+          <Link to="Al-Khwarizmi">al-Khwarizmi, Muhammad ibn Musa</Link>
+        </li>
+        <li>Babbage, Charles</li>
+        <li>Berners-Lee, Tim</li>
+        <li>Bush, Vannevar</li>
+        <li>Dijkstra, Edsger</li>
+        <li>Englebart, Douglas</li>
+        <li>Hopper, Grace</li>
+        <li>Johnson, Katherine</li>
+        <li>Knuth, Donald</li>
+        <li>Lovelace, Ada</li>
+        <li>Shannon, Claude</li>
+        <li>Torvalds, Linus</li>
+        <li>Turning, Alan</li>
+        <li>von Neumann, John</li>
+        <li>Wing, Jeanette</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
